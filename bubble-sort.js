bd98b4683ed53d8d2d7831307a213bdc0f63c102
Dancode-188 <danbitengo@gmail.com>
2024-09-27 17:43:10 +0300

    Refactor bubble sort algorithm for improved efficiency

--- bubble-sort.js
+++ bubble-sort.js
@@ -7,11 +7,25 @@ function bubbleSort(arr) {
         // Swap those values
 
         // Do not move this console.log
-        console.log(arr.join(","));
+        // console.log(arr.join(","));
 
     // If you get to the end of the array and no swaps have occurred, return
 
     // Otherwise, repeat from the beginning
+    let hasSwap = true;
+    while(hasSwap){
+        hasSwap = false;
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] > arr[i + 1]){
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                hasSwap = true;
+                console.log(arr.join(","));
+            }
+        }
+    }
+    return arr;
 
 }
 
